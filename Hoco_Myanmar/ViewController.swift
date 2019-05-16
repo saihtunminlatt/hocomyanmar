3527a020618f5cb5b52f8ca45bb6e8c52457a98e
Htun Min Latt <htunminlatt.iosdev@gmail.com>
2019-05-16 11:50:29 +0630

    Initial Commit

---
+++ Hoco_Myanmar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hoco_Myanmar
+//
+//  Created by ALAY on 5/16/19.
+//  Copyright © 2019 Aerial-Yangon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
